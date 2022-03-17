c59ae7f939cbb673d62e69200d466f844e07d7bf
Marcos <marcos_fmendes@hotmail.com>
2022-03-17 09:07:35 -0300

    Curriculo Button Debug

--- src/components/Home/DownloadButton.js
+++ src/components/Home/DownloadButton.js
@@ -29,7 +29,7 @@ function DownloadButton({
       <a
         className="button"
         to={btnLink}
-        href="https://doc-08-8c-docs.googleusercontent.com/docs/securesc/i6fakgd7f8c2sothg162f7gin0gubuai/g0pb4q5gf03k0irnlrmvfg4setrl9cfv/1647460650000/06159338018623915181/06159338018623915181/1mOdq3xRBg51IdunnfrelBrXnkDcvUtay?e=download&ax=ACxEAsZB0brgVr3-LX8HkjjrGRALo7Agz4sd8sQihUNv5YS8vK0QHUIyFvp12kv-rwn3cORMnx4vQFv_xgvws8Jl-oiMV_cI7XcIDyvuYjrNSnHfp_7TvRDNxuj0JOoVDrs2P_8RHgEgeZFWwidbUvdaTfXTfYSVtfwBD69ObM8osmlnqg0-V0QrlAnWXAAGpZDKfbLYJnHJWLQcWuf_Zmn25IUPBBbD-yycs7B4r6fWb2SKXLBWwZti81beZfem48-Zdw577370jSLdey4yzsLoH6N6pYOetoZdfq6bk14L3ZBic030D60CGRzhpQMREsYD4DUo5iL4qKisESnoVCuCVb_ryZ-8w6IYeUQDkF2C_LuRLTPZNZKyGojCkGw75oxht5yQXIaxGwJclaw1H9s86rdXqaNaN18gLvZVFjMplNqn6AI-oKXiQuELCmfmxZR7EBJae5NZAXDMNwZpTc5KFNeCP9c8pNqX897iTo0WHWXE6Nyi41A4PwX_wB1R0rnxbTeGfHF0fSZA6HTztqNtbfzus501i6udeGEP9SdmfCzWYAASRzWh7U2LkdiDRiyZz9CL_c45ls50bvcb4jW1fj1jMcW1E5A7e8ua5_-UlcZ8UMDFX0Iani2AmsvZeoxfLJ-1yDPq790iX8513_2ixqCoEOozjNhWuQlfRGsb&authuser=0"
+        href="https://drive.google.com/u/0/uc?id=16EJCJoUQk-5exCSnYf_DcAnKAz9o33ya&export=download"
       >
         {btnText}
       </a>
