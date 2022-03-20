5943c639c50a792bfc921359f5e66cb4606875a5
Marcos <marcos_fmendes@hotmail.com>
2022-03-20 17:05:35 -0300

    Projects Order

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -3,16 +3,16 @@ import Dog from "../images/dog.jpg";
 
 const projects = [
   {
-    name: "Tractian Project",
-    desc: "Aplicação que mostra todos os dados e ações possíveis  criados no back-end para o usuário, utilizando React e Highcharts.",
-    img: UTrackerImg,
-    href: "",
-  },
-  {
     name: "Project Dogs",
     desc: "Aplicação responsável pela criação de uma rede social de fotos de cachorros utilizando React, Regex, JavaScript e CSS.",
     img: Dog,
     href: "https://app-dogss.netlify.app",
+  },
+  {
+    name: "Tractian Project",
+    desc: "Aplicação que mostra todos os dados e ações possíveis  criados no back-end para o usuário, utilizando React e Highcharts.",
+    img: UTrackerImg,
+    href: "",
   },
 ];
 
